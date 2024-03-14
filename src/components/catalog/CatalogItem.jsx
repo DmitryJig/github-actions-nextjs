0682da14b48435021bd3269ea57ca2278fd01a03
dima <jd2344430@gmail.com>
2024-03-14 21:29:10 +0500

    add catalog items-3

--- src/components/catalog/CatalogItem.jsx
+++ src/components/catalog/CatalogItem.jsx
@@ -5,7 +5,8 @@ const CatalogItem = ({product}) => {
             <Link href={`/catalog/${product.id}`}>
             <div className="bg-gray-500 border-2 border-stone-300 rounded-lg hover:bg-gray-600 mt-3 p-3">
                 {product.title}
-                <p>Цена: {product.price} руб.</p>
+                <p>Цена: руб.</p>
+                <p>{product.price}</p>
             </div>
             </Link>
         
